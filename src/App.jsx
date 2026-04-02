348434cbad1b4264978903a28c3236a4fb8a90de
FirdavsAbdullayev <firdavsabdullayev622@gmail.com>
2026-04-02 10:12:02 +0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const data = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "Fully Rounded",
+      data: [-80, -70, 5, -25, 90, 35, 40],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      borderRadius: 20, // 🔥 rounded effekt
+    },
+    {
+      label: "Small Radius",
+      data: [-90, 60, 2, 30, -45, -85, -75],
+      backgroundColor: "rgba(54, 162, 235, 0.5)",
+      borderRadius: 5,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
 
 const App = () => {
   return (
-    <div>
-      <BarChart
-        height={300}
-        series={[
-          { ...seriesA, stack: 'total' },
-          { ...seriesB, stack: 'total' },
-          { ...seriesC, stack: 'total' },
-        ]}
-      />
+    <div style={{ width: "800px", margin: "auto" }}>
+      <Bar data={data} options={options} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
